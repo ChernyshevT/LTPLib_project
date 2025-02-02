c5c10245e5cb1e2beaab027be3aae9835ad6977f
Chernyshev Timofey <chernyshev.tv@ihed.ras.ru>
2025-02-02 10:44:37 +0530

    new file:   examples/csections_db/CH4.py 	new file:   examples/csections_db/CH4/ELASTIC.txt 	new file:   examples/csections_db/CH4/INELASTIC.txt 	new file:   examples/run_localsim.py 	modified:   README.md 	modified:   _ltplib/src/_ltplib.cxx 	modified:   _ltplib/src/def_csection_set.cxx 	modified:   _ltplib/src/def_csection_set.hxx 	modified:   _ltplib/src/def_csection_set_cfg.cxx 	modified:   _ltplib/src/def_grid_cfg.cxx 	modified:   _ltplib/src/io_strings.hxx 	modified:   examples/util/__init__.py 	modified:   examples/util/args.py 	modified:   examples/util/frames.py 	modified:   examples/util/loggers.py 	modified:   examples/util/plots.py

---
+++ examples/csections_db/CH4.py
@@ -0,0 +1,115 @@
+# database for c+CH4 interactions
+[
+	{"TYPE":"BACKGROUND", "KEY":"CH4",
+	 "MASSRATE":3.420074282530393e-05},
+	
+	##############################################################################
+	# A critical evaluation of low-energy electron impact cross sections for
+	# plasma processing modeling. II: Cl4, SiH4, and CH4
+	# doi: 10.1007/BF01447255, cite: morgan1992
+	#
+	# Cross Sections for Electron Collisions with Methane
+	# doi: 10.1063/1.4918630,  cite: song2015
+	{"TYPE":"ELASTIC",
+	 "REF":r"\cite{0.1007/BF01447255,song2015}",
+	 "CSEC":(f"{fpath}/CH4/ELASTIC.txt",
+	 {"search":"DATABASE:         Morgan (Kinema Research  Software)"}),
+	 "MTCS":(f"{fpath}/CH4/ELASTIC.txt",
+	 {"search":"DATABASE:         Community database"}),
+	},
+
+	{"TYPE":"ATTACHMENT",   "THRESHOLD":6.0, "COMMENT":"CH3+H^-",
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt",
+	  dict(search="CH4 -> CH3+H^-", exterp=0)),
+	},
+	{"TYPE":"ATTACHMENT",   "THRESHOLD":6.0, "COMMENT":"H2+CH2^-",
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt",
+	  dict(search="CH4 -> H2+CH2^-", exterp=0)),
+	},
+
+	{"TYPE":"ROTATIONAL",   "THRESHOLD":0.0078,
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt",
+	  dict(search="0.0078 / threshold energy", exterp=0)),
+	},
+	{"TYPE":"ROTATIONAL",   "THRESHOLD":0.0130,
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt",
+	  dict(search="0.013 / threshold energy", exterp=0)),
+	},
+	 
+	{"TYPE":"VIBRATIONAL",  "THRESHOLD":0.160,
+	 "COMMENT": "V24",
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt", 
+	  dict(search="CH4 -> CH4(V24)(0.162eV)")),
+	},
+	{"TYPE":"VIBRATIONAL",  "THRESHOLD":0.362,
+	 "COMMENT": "V13",
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt",
+	  dict(search="CH4 -> CH4(V13)(0.362eV)")),
+	},
+	 
+	##############################################################################
+	# Set of the Electron Collision Cross Sections for Methane Molecule
+	# doi:  10.1109/TPS.2018.2885610, cite: gadoum2018
+	{"TYPE":"DISSOCIATION", "THRESHOLD":8.8,
+	 "COMMENT":"CH3+H", "REF":r"\cite{gadoum2018}",
+	 "CSEC": lambda en, th, a=[4.9441,0.3863,1.4973,1.2794]: 
+	  1e-20*(a[0]*(th/en)**a[1] * (1 - (th/en)**a[2])**a[3]).real
+	},
+	{"TYPE":"DISSOCIATION", "THRESHOLD":9.4,
+	 "COMMENT":"CH2+H2", "REF":r"\cite{gadoum2018}",
+	 "CSEC": lambda en, th, a=[1.2903,0.5079,1.3305,1.2262]: \
+	  1e-20*(a[0]*(th/en)**a[1] * (1 - (th/en)**a[2])**a[3]).real
+	},
+	{"TYPE":"DISSOCIATION", "THRESHOLD":12.5,
+	 "COMMENT":"CH+H2+H", "REF":r"\cite{gadoum2018}",
+	 "CSEC": lambda en, th, a=[0.7185,0.6362,1.5057,1.2796]: \
+	  1e-20*(a[0]*(th/en)**a[1] * (1 - (th/en)**a[2])**a[3]).real
+	},
+	{"TYPE":"DISSOCIATION", "THRESHOLD":14.0,
+	 "COMMENT":"C+2H2", "REF":r"\cite{gadoum2018}\footnote{Something is wrong with analytical approximation, data-points were used instead.}",
+	# something is wrong with analytical approx!
+	# "CSEC": lambda en, th=14.0, a=[0.5681,0.8807,4.0006,1.1164]: \
+	#  1e-20*(a[0]*(th/en)**a[1] * (1 - (th/en)**a[2])**a[3]).real
+	 "CSEC":(f"{fpath}/CH4/INELASTIC.txt",
+	  dict(search="CH4 -> C+H2+H2(14eV-dissoc)"))
+	},
+	
+	##############################################################################
+	# Cross Sections for Electron Collisions with Methane
+	# doi:  10.1063/1.4918630, cite: song2015
+	{"TYPE":"IONIZATION",   "THRESHOLD":12.63, "OPBPARAM":7.3,
+	 "COMMENT":"e+CH4^+", "REF":r"\cite{song2015}",
+	 "CSEC": lambda en, th, a=[1.8034,-1.4809,-3.8281,17.892,-30.666,16.38]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+	{"TYPE":"IONIZATION",   "THRESHOLD":12.63, "OPBPARAM":7.3,
+	 "COMMENT":"e+CH3^+", "REF":r"\cite{song2015}",
+	 "CSEC":lambda en, th, a=[1.5636,-1.3767,-1.7262,11.6948,-23.1158,13.6104]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+	{"TYPE":"IONIZATION",   "THRESHOLD":16.20, "OPBPARAM":7.3,
+	 "COMMENT":"e+CH2^+", "REF":r"\cite{song2015}",
+	 "CSEC":lambda en, th, a=[0.2133,-0.2194,-0.1853,0.8266,-0.1393,0.0044]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+	{"TYPE":"IONIZATION",   "THRESHOLD":22.20, "OPBPARAM":7.3,
+	 "COMMENT":"e+CH^+", "REF":r"\cite{song2015}",
+	 "CSEC":lambda en, th, a=[-0.1661,0.1893,-0.3884,4.0615,-5.8045,3.2324]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+	{"TYPE":"IONIZATION",   "THRESHOLD":22.00, "OPBPARAM":7.3,
+	 "COMMENT":"e+C^+", "REF":r"\cite{song2015}",
+	 "CSEC":lambda en, th, a=[-0.1234,0.0362,0.5527,-0.6303,0.5648,-0.1526]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+	{"TYPE":"IONIZATION",   "THRESHOLD":22.30, "OPBPARAM":7.3,
+	 "COMMENT":"e+H2^+", "REF":r"\cite{song2015}",
+	 "CSEC":lambda en, th, a=[-0.0058,0.0088,-0.077,0.2865,0.1644,-0.2252]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+	{"TYPE":"IONIZATION",   "THRESHOLD":21.10, "OPBPARAM":7.3,
+	 "COMMENT":"e+H2^+", "REF":r"\cite{song2015}",
+	 "CSEC":lambda en, th, a=[-0.4317,0.3519,1.4791,-5.5021,11.5604,-4.6928]: \
+	 1e-17/th/en * (a[0]*log(en/th) + sum(a*(1-th/en)**j for j,a in enumerate(a[1:], 1)))
+	},
+]
---
+++ examples/run_localsim.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python3
+
+import sys, os, timeit, shutil, logging, signal
+import numpy   as np
+import _ltplib as ltp
+
+from datetime     import datetime
+from importlib    import import_module
+from util.frames  import *
+from util.loggers import *
+from util.args    import *
+
+################################################################################
+def handle_sigterm(n, frame):
+	raise SystemExit(signal.strsignal(n))
+
+################################################################################
+def inject_parts (pstore, npp_in, x0, x1, vt):
+	distro  = np.empty([npp_in, 4], dtype=np.float32)
+	# pxy
+	distro[:, 0] = np.random.uniform(x0, x1, size=npp_in)
+	# vxyz
+	distro[:, 1] = np.random.normal(size=npp_in, scale=vt)
+	distro[:, 2] = np.random.normal(size=npp_in, scale=vt)
+	distro[:, 3] = np.random.normal(size=npp_in, scale=vt)
+	# inject
+	pstore.inject({"e":distro})
+	# print(pstore.index)
+
+################################################################################
+
+def main(args):
+	ltp.load_backend("default");
+	
+	import time
+	from calcs import ve
+	
+	for k,v in vars(args).items():
+		if v is None:
+			continue
+		if type(v) != str:
+			logger.info(f"{k:<8} : {v}")
+		else:
+			logger.info(f"{k:<8} : \"{v}\"")
+
+	##############################################################################
+	# init csection database:
+	bginfo = [(lambda x: (x[0], float(x[1])))(x.split(":")) for x in args.bginfo]
+	keys, fracs = zip(*bginfo); fracs = np.array(fracs)/sum(fracs)
+	cs_cfg = [
+	{ "TYPE":"PARTICLE", "KEY":"e"
+	, "ENCFFT": 2.842815e-16}, #CGS (cm/s)² -> eV
+	]
+	cs_kws = {
+	 "max_energy" : args.max_energy,
+	 "ptdescr"    : "e",
+	 "rescale"    : 1e4, #CGS (m² -> cm²)
+	}
+	for key in keys:
+		fname = f"csections_db/{key}.py"
+		with open(fname) as f:
+			cs_cfg += eval(f.read() \
+			, {"fpath":os.path.dirname(fname), **vars(import_module("math"))})
+	cset = ltp.csection_set(cs_cfg, **cs_kws)
+
+	##############################################################################
+	E0 = args.EN*args.nbg*1e-17 #SI Td -> V/cm
+	B0 = args.BN*args.nbg*1e-17 #SI Hx -> Gauss
+	
+	chinfo = []
+	for entry, info in zip(cset, cset.chinfo):
+		descr = info
+		if entry.comment:
+			descr += f" {entry.comment}"
+		if entry.enth > 0:
+			descr += f" ({entry.enth:.2e} eV)"
+		chinfo.append(descr)
+	
+	cfg = {
+			 "units"  : "CGS",
+			 "TE"     : args.te,
+			 "N0"     : args.nbg,
+			 "EN"     : args.EN,
+			 "BN"     : args.BN,
+			 "E0"     : E0,
+			 "B0"     : B0,
+			 "fracs"  : fracs, 
+			 "tstep"  : args.dt,
+			 "tframe" : args.dt*args.nsub,
+			 "chinfo" : chinfo,
+	}
+	for k,v in cfg.items():
+		unitx = {"N0":" 1/cm3", "EN":" Td", "BN":" Hx", "E0":" V/cm", "B0":" G"}.get(k,"") #CGS
+		logger.info(f"{k:<8} : {v}"+unitx)
+	##############################################################################
+	dt = args.dt
+	##############################################################################
+	# create fake 1d grid
+	grid = ltp.grid(1 
+	, step=[1]
+	, axes=[[*range(args.nodes+1)]]
+	, nodes=[[i] for i in range(args.nodes)]
+	, loopax="x")
+
+	##############################################################################
+	# init particle storage
+	import consts
+	pstore_cfg = {
+		"npmax" : int(args.npp*(1.0 + args.extra)/args.nodes),
+		"ptinfo": [
+			{"KEY":"e", "CHARGE/MASS": -5.272810e+17}, #CGS
+		],
+	}
+	pstore = ltp.pstore(grid, **pstore_cfg)
+	##############################################################################
+	# declare value caches and corresponding numpy-arrays
+	emfield = ltp.vcache(grid, "f32", vsize=2, order=1)
+	bgrnd   = ltp.vcache(grid, "f32", vsize=len(cset.bglist), order=0)
+	cfreq   = ltp.vcache(grid, "u32", vsize=len(cset), order=0)
+
+	g_emfield = np.zeros(**emfield.cfg)
+	g_cfreq   = np.zeros(**cfreq.cfg)
+	m_cfreq   = np.zeros(**{**cfreq.cfg, **{"dtype":np.float32}})
+
+	g_emfield[..., 0] = -E0/2.99792458e2  # (V/cm -> statV/cm) #CGS
+	g_emfield[..., 1] = +B0/2.99792458e10 # (G -> G*s/cm)      #CGS
+	ltp.bind_remap_fn(emfield, "<", g_emfield)()
+	
+	g_bgrnd   = np.ones (**bgrnd.cfg)
+	for j, frac in enumerate(fracs):
+		g_bgrnd[..., j] = args.nbg*frac
+	ltp.bind_remap_fn(bgrnd, "<", g_bgrnd)()
+	
+	##############################################################################
+	ppush_fn = ltp.bind_ppush_fn (pstore, "ExBz", emfield)
+	order_fn = ltp.bind_order_fn (pstore)
+	mcsim_fn = ltp.bind_mcsim_fn (pstore, cfreq, cset, bgrnd)
+	remap_cfreq_out = ltp.bind_remap_fn (cfreq, ">", g_cfreq)
+	remap_cfreq_in  = ltp.bind_remap_fn (cfreq, "<", g_cfreq)
+
+	##############################################################################
+	fname = f"{args.save}.zip"
+	if args.save and os.path.exists(fname):
+		if not args.run:
+			if input(f"\tdir \"{fname}\" already exists, delete it? [y] ") == "y":
+				os.remove(fname)
+			else:
+				exit(0)
+		else:
+			raise RuntimeError(f"dir \"{fname}\" already exists!")
+	##############################################################################
+	if args.run == False or not (args.run or input(f"run? [y|yes] or.. ") in ["y", "yes"]):
+		exit(0)
+	
+	tdescr = ["mcsim_fn","ppush_fn"]
+	ts = np.empty([1+len(tdescr)], dtype=np.float64)
+
+	# inject samples
+	logger.info("inject samples..")
+	inject_parts(pstore, args.npp, 0, args.nodes, np.sqrt(args.te/2.842815e-16)) #CGS
+	
+	# main cycle
+	logger.info("start calculation..")
+	for irun in range(1, args.nrun+1):
+		
+		t0, t1 = (irun-1)*args.nsub*args.dt, irun*args.nsub*args.dt
+		logger.info(f"frame#{irun:06d} ({t0*1e9:10.4f} -> {t1*1e9:10.4f}ns)..")
+		
+		iclc, tclc = 0, time.time()
+		# start frame [t --> t + dt*args.nsub], clear data
+		g_cfreq[...] = 0; remap_cfreq_in()
+		
+		ts[:] = 0.0; j1 = 0
+		for isub in range (1, args.nsub+1):
+			
+			seed = np.random.randint(0xFFFFFFFF, dtype=np.uint32)
+			ts[1] += mcsim_fn(dt, seed)().dtime
+			
+			ts[2] += ppush_fn(dt)().dtime; order_fn()()
+			
+			npp = len(pstore); j1 += npp
+		
+		# end frame
+		tclc = time.time()-tclc
+		logger.info(f"time {'frame':<10} : {tclc:>6.2f} s, ({npp} samples)")
+		for key,tval in zip(tdescr, ts[1:]):
+			logger.info(f"time {key:<10} : {tval:>6.2f} s, {tval/j1*1e9:>6.2f} ns/sample")
+		
+		pts, _ = pstore.extract()
+		
+		# keep ensemble's size constant
+		if args.resample and args.npp != npp:
+			pstore.reset()
+			np.random.shuffle(pts)
+			pstore.inject({"e": pts[:min(args.npp, npp)]})
+			if args.npp > npp:
+				pstore.inject({"e": pts[:args.npp-npp]})
+			logger.info(f"resample {npp} -> {args.npp}")
+		
+		remap_cfreq_out();
+		outv = {
+			"pdata": pts[:, 1:],
+			"cfreq": np.sum(g_cfreq, axis=0, dtype=np.float32)/j1/dt,
+			"fluid": {
+				# cm/s
+				"u0": np.sqrt(np.mean(np.sum(pts[:, 1:]**2, axis=1))),
+				"ux": np.mean(pts[:, 1]),
+				"uy": np.mean(pts[:, 2]),
+				"uz": np.mean(pts[:, 3]),
+			},
+			"cfg": {**cfg,
+				"tindex": [(irun-1)*args.nsub, irun*args.nsub],
+			},
+		}
+		for k,v in outv["fluid"].items():
+			logger.info(f"fluid.{k} = {v:+e} cm/s") #CGS
+		logger.info(f"fluid.emean =  {(outv['fluid']['u0']**2) * 2.842815e-16:e} eV")
+		
+		for info,freq in zip(cset.chinfo, outv['cfreq']):
+			logger.info(f"{info:<48}{freq:e}")
+		
+		# write frame
+		if fpath := args.save:
+			fname = f"{fpath}/frame{irun:06d}.zip"
+			save_frame(fname, "w", **outv)
+
+################################################################################
+args = {
+	"--bginfo": {
+		"help"     : "list with KEY:FRACTION separated by spaces",
+		"nargs"    : "+",
+		"type"     : str,
+		"required" : True,
+	},
+	"--nbg": {
+		"help"     : "sum of backgrounds' densities, (1/cm^3)", #CGS
+		"type"     : float,
+		"default"  : 1e17,
+		"action"   : check_arg(lambda x: x>0, "should be positive"),
+	},
+	"--EN": {
+		"help"     : "reduced electric field (E_x/n_0, Td)",
+		"type"     : float,
+		"required" : True,
+	},
+	"--BN": {
+		"help"     : "reduced magnetic field (B_z/n_0, Hx)",
+		"type"     : float,
+		"default"  : 0,
+	},
+	"--max_energy" : {
+		"help"     : "energy limit for cross-section database, (eV)",
+		"type"     : float,
+		"required" : True,
+		"action"   : check_arg(lambda x: x>0, "should be positive"),
+	},
+	"--te": {
+		"help" : "initial temperature of electron ensemble",
+		"type": float,
+		"default": 0,
+		"action"   : check_arg(lambda x: x>0, "should be positive"),
+	},
+	"--dt": {
+		"help"     : "simulation's time-step (t->t+dt)",
+		"type"     : float,
+		"default"  : 2.5e-12,
+	},
+	"--nodes": {
+		"help"     : "number of cpu-nodes (2 at least)",
+		"type"     : int,
+		"default"  : 1,
+		"action"   : check_arg(lambda x: x>0, "{} <= 0"),
+	},
+	"--nsub": {
+		"help": "number of sub-steps per frame (t -> t+dt*nsub)",
+		"type": int,
+		"default" : 2,
+		"action"   : check_arg(lambda x: x>=2, "{} < 2"),
+	},
+	"--nrun": {
+		"help": "number of frames to calculate (t -> t+dt*nsub*nrun)",
+		"type": int,
+		"default": 1,
+	},
+	"--npp": {
+		"help": "number of samples",
+		"type": int,
+		"default": 100000,
+	},
+	"--extra": {
+		"help"     : "extra storage capacity (multiplication factor), some minimal value is required for even if there are no ionization events", 
+		"type"     : float,
+		"default"  : 0.25,
+		"action"   : check_arg(lambda x: x>0, "sould not be zero"),
+	},
+	"--resample" : {
+		"help": "resample eVDF each frame to keep size of ensemble constant",
+		"action": argparse.BooleanOptionalAction,
+	},
+	"--save": {
+		"help"     : "path to save the results, default=\"\" (do not save)",
+		"type"     : str,
+		"default"  : "",
+	},
+	"--run": {
+		"help": "run simulation without asking",
+		"action": argparse.BooleanOptionalAction,
+	},
+	"--loglevel": {
+		"help": "logging level: DEBUG|INFO|WARNING|ERROR",
+		"type": str,
+		"required": False,
+		"default": "INFO",
+	},
+}
+
+################################################################################
+if __name__ == '__main__':
+	
+	args   = parse_args(sys.argv, args)
+	logger = get_logger()
+	setup_logging(level=args.loglevel.upper(), root=True)
+	
+	signal.signal(signal.SIGTERM, handle_sigterm)
+	try:
+		# check directory
+		if args.save and os.path.isdir(args.save):
+			if not args.run:
+				if input\
+				(f"dir \"{args.save}\" already exists, delete contents? [y|yes] or.. ")\
+				in ["y", "yes"]:
+					shutil.rmtree(args.save)
+				else:
+					exit(0)
+			else:
+				raise RuntimeError(f"dir \"{args.save}\" already exists!")
+		
+		# create log-file
+		if args.save:
+			os.makedirs(args.save, exist_ok=True)
+			fname\
+			= f"{args.save}/runat-{datetime.now().strftime('%Y-%m-%d-%H-%M')}.log"
+			
+			logger.info(f"will write to \"{fname}\"")
+			setup_logging(level=args.loglevel.upper(), root=True\
+			, handler = logging.FileHandler(fname, "a", delay=False))
+		
+		# run program
+		sys.exit(main(args))
+		
+	except (KeyboardInterrupt, SystemExit):
+		logger.info("manual exit")
+		sys.exit(0)
+	
+	except Exception as err:
+		import colorama
+		msg = f"{colorama.Fore.RED+colorama.Style.BRIGHT+colorama.Back.BLACK}{err}{colorama.Style.RESET_ALL}"
+		logger.exception(msg)
+		sys.exit(1)
+
