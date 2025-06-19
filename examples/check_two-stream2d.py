4c34e94046f370a2d37bba2369153f5d98422657
Chernyshev Timofey <chernyshev.tv@ihed.ras.ru>
2025-06-20 03:12:17 +0400

    modified:   examples/check_two-stream2d.py

--- examples/check_two-stream2d.py
+++ examples/check_two-stream2d.py
@@ -12,28 +12,24 @@ def main(args):
 	for arg in args:
 		
 		pdata, frame = load_frame(arg), load_frame(arg.replace("pdata", "frame"))
-		
-		nx,ny = frame.cfg.units
-		dx,dy = frame.cfg.step
+		nd = pdata.shape[1]-3
 		t0,t1 = [frame.cfg.dt*k for k in frame.cfg.tindex]
-		ng    = frame.cfg.order
 		
-		Ex,Ey = np.gradient(frame.vplasma, dx, dy, edge_order=2)
-		en_field = np.mean((Ex*Ex + Ey*Ey)/8/np.pi)*2.99792458e2
+		grads = np.gradient(frame.vplasma, *frame.cfg.step, edge_order=2)
+		en_field = np.mean(sum(grad**2 for grad in grads))*2.99792458e2/8/np.pi
 		vp_range = np.max(np.abs(frame.vplasma))*2.99792458e2
 		
 		j0,j1,j2 = pdata.index
-		xs = pdata.data[j0:j1, 0]
-		ys = pdata.data[j0:j1, 1]
-		vx = pdata.data[j0:j1, 2]
-		vy = pdata.data[j0:j1, 3]
+		vx = pdata.data[j0:j1, nd+0]
+		vy = pdata.data[j0:j1, nd+1]
+		vz = pdata.data[j0:j1, nd+2]
 		
-		# ~ key = arg.replace("pdata","fstat").replace(".zip","") 
 		key = f"stat{frame.cfg.tindex[1]:06d}"
 		dset[key] = {
 		 "cfg"      : frame.cfg,
 		 "en_exx"   : np.mean(vx*vx)*2.842815e-16,
 		 "en_eyy"   : np.mean(vy*vy)*2.842815e-16,
+		 "en_ezz"   : np.mean(vz*vz)*2.842815e-16,
 		 "en_field" : en_field,
 		 "vp_range" : vp_range,
 		}
@@ -41,13 +37,14 @@ def main(args):
 			dset[key].update({"verrs" : frame.verrs})
 		
 		if frame.cfg.ions:
-			vx = pdata.data[j1:j2, 2]
-			vy = pdata.data[j1:j2, 3] 
+			vx = pdata.data[j1:j2, nd+0]
+			vy = pdata.data[j1:j2, nd+1] 
+			vz = pdata.data[j1:j2, nd+1] 
 			dset[key].update({
 			 "en_ixx" : np.mean(vx*vx)*5.182139e-13,
 			 "en_iyy" : np.mean(vy*vy)*5.182139e-13,
+			 "en_izz" : np.mean(vz*vz)*5.182139e-13,
 			})
-		# ~ print(dset.keys)
 	
 	fpath = os.path.dirname(os.path.abspath(arg))
 	save_frame(f"{fpath}/dset.zip", "w", **dset)
